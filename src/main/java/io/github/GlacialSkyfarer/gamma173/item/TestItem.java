628fa564da929b0b042fd1d10588d20bb369187f
GlacialSkyfarer <curseflame1122@gmail.com>
2025-09-22 23:13:07 -0600

    Added ALL vanilla block descriptions (And the item description as a whole!) Many blocks will die. But that's a sacrifice I'm willing to make.

---
+++ src/main/java/io/github/GlacialSkyfarer/gamma173/item/TestItem.java
@@ -0,0 +1,10 @@
+package io.github.GlacialSkyfarer.gamma173.item;
+
+import net.modificationstation.stationapi.api.template.item.TemplateItem;
+import net.modificationstation.stationapi.api.util.Identifier;
+
+public class TestItem extends TemplateItem {
+    public TestItem(Identifier identifier) {
+        super(identifier);
+    }
+}
